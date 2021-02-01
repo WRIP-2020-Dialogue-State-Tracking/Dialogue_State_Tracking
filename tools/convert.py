52b4fe51ee8ce069a6fa19ea7136442b6806899b
Calm Cookie <singla.uday200@gmail.com>
2021-02-01 23:34:08 +0530

    Convert metadata
    Solves https://github.com/WRIP-2020-Dialogue-State-Tracking/Dialogue_State_Tracking/issues/2

--- tools/convert.py
+++ tools/convert.py
@@ -41,13 +41,101 @@ def modifyDialogAct(dialog_act, dictionary, modifiers) -> str:
                         continue
     return error, error_info, modify_info
 
+def modifyMetadata(metadata, dictionary, selected_domains):
+    error = False
+    error_info = []
+    for domain in metadata:
+        if domain in selected_domains:
+            for key in metadata[domain]:
+                for slot in metadata[domain][key]:
+                    if slot == 'booked':
+                        for booked_index, booked_dict in enumerate(metadata[domain][key][slot]):
+                            skip = False
+                            for booked_key in booked_dict:
+                                values = booked_dict[booked_key]
+                                if isinstance(values, list):
+                                    for value in values:
+                                        try:
+                                            metadata[domain][key][slot][booked_index][booked_key] = dictionary[str(value).lower()]
+                                            skip = True
+                                            break
+                                        except:
+                                            if (
+                                                re.match("\d{2}:\d{2}", value)
+                                                or value.isdigit()
+                                                or re.match("\d{1}:\d{2}", value)
+                                                or value == "none"
+                                                or value == "dontcare"
+                                            ):
+                                                skip = True
+                                                break
+                                    if skip == False and metadata[domain][key][slot] != []:
+                                        error = True
+                                        error_info.append({domain: {
+                                            key: {
+                                                slot: metadata[domain][key][slot]
+                                            }
+                                        }})
+                                else:
+                                    value = values    
+                                    try:
+                                        metadata[domain][key][slot][booked_index][booked_key] = dictionary[str(value).lower()]
+                                        break
+                                    except:
+                                        if (
+                                            re.match("\d{2}:\d{2}", value)
+                                            or value.isdigit()
+                                            or re.match("\d{1}:\d{2}", value)
+                                            or value == "none"
+                                            or value == "dontcare"
+                                            or booked_key == "reference"
+                                        ):
+                                            continue
+                                        else:
+                                            error = True
+                                            error_info.append({domain: {
+                                                key: {
+                                                    slot: metadata[domain][key][slot]
+                                                }
+                                            }})
+                                            continue
+                    else:
+                        skip = False
+                        for value in metadata[domain][key][slot]:
+                            try:
+                                metadata[domain][key][slot] = dictionary[str(value).lower()]
+                                skip = True
+                                break
+                            except:
+                                if (
+                                    re.match("\d{2}:\d{2}", value)
+                                    or value.isdigit()
+                                    or re.match("\d{1}:\d{2}", value)
+                                    or value == "none"
+                                    or value == "dontcare"
+                                ):
+                                    skip = True
+                                    break
+                        if skip == False and metadata[domain][key][slot] != []:
+                            error = True
+                            error_info.append({domain: {
+                                key: {
+                                    slot: metadata[domain][key][slot]
+                                }
+                            }})
+                        
+    return error, error_info
 
-def convertDialogActs(
+
+
+
+def convertDialogActsAndMetadata(
     dataset_of_domain: typings.dataframe,
     conversion_files: List[str],
     modifiers: List[str],
+    selected_domains: List[str]
 ):
-    """Converts a dialog act of a Dataframe into another language
+    """Converts dialog acts and meta data of a Dataframe into another language
 
     Notes:
 
@@ -60,6 +148,7 @@ def convertDialogActs(
       -  `path_to_conversion_file` : Path to xlsx file containing mapping from english to other langugage.
       -  `ontology` : Array containing words in ontology. eg `departure`
       -  `modifiers` : Array containing fields to be modified e.g. `Taxi-Inform`.
+      -  `selected_domains` : Array containing selected domains
     """
     dictionary = dict()
     for file in conversion_files:
@@ -81,6 +170,7 @@ def convertDialogActs(
                      "database/attraction-variants.json"]
     )
     errors = dict()
+    metadata_errors = dict()
     for index in dataset_of_domain.index:
         for ind, dialog in enumerate(dataset_of_domain["log"][index]):
             err, error_info, modify_info = modifyDialogAct(
@@ -89,6 +179,17 @@ def convertDialogActs(
             dialog["text"] = convert_text(modify_info, dialog["text"])
             if err:
                 errors.setdefault(index, []).append(error_info)
+            
+            if dialog['metadata'] == {}:
+                continue
+            metadata_err, metadata_error_info = modifyMetadata(
+                dialog["metadata"], dictionary, selected_domains
+            )
+            if metadata_err:
+                metadata_errors.setdefault(index, []).append(metadata_error_info)
+
+    with open("metadata-errors.json", "w") as f:
+        json.dump(metadata_errors, f, indent=2)   
     with open("conversion-errors.json", "w") as f:
         json.dump(errors, f, indent=2)
     print(
